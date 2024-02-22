34d43e6fbaa698a884561f50c989f7a6d94120d5
PetxCode <brighterdayscodelab@gmail.com>
2024-02-22 22:19:43 +0100

    work

--- index.ts
+++ index.ts
@@ -23,7 +23,10 @@ const portServer = process.env.PORT!;
 const port = parseInt(portServer);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", process.env.APP_URL_DEPLOY);
+  res.header(
+    "Access-Control-Allow-Origin",
+    process.env.APP_URL_DEPLOY || process.env.APP_URL
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
@@ -40,7 +43,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 // });
 app.use(
   cors({
-    origin: process.env.APP_URL_DEPLOY,
+    origin: process.env.APP_URL_DEPLOY || process.env.APP_URL,
   })
 );
 app.use(express.json());
