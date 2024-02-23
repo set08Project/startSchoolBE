56ca71faf60238dd88923db901c3d885b117d9f6
PetxCode <brighterdayscodelab@gmail.com>
2024-02-23 07:35:03 +0100

    adding

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ const portServer = process.env.PORT!;
 const port = parseInt(portServer);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", process.env.APP_URL_DEPLOY);
+  res.header("Access-Control-Allow-Origin", "https://just-next.web.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
