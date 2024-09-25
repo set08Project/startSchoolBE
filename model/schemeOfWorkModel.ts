0b0735957e09cb02cffe3f401203d73d9f1a3ac9
Ayomide24500 <Ayomideadisa83@gmail.com>
2024-09-25 17:09:24 +0100

    schema-backend-done

--- model/schemeOfWorkModel.ts
+++ model/schemeOfWorkModel.ts
@@ -1,59 +1,48 @@
 import { Document, Schema, model } from "mongoose";
 
+
 interface LearningItem {
   id: string;
-  value: Array<{}>;
+  value: Array<any> | string; 
 }
+
 
 interface iScheme {
   weeks: string;
   subject: string;
   classType: string;
-  topics: string ;
+  topics: Array<any>; 
   term: string;
-  learningObject: Array<LearningItem>; 
-  learningActivities: Array<LearningItem>; 
-  embeddedCoreSkills: Array<LearningItem>; 
-  learningResource: Array<LearningItem>; 
+  learningObject: Array<LearningItem>;
+  learningActivities: Array<LearningItem>;
+  embeddedCoreSkills: Array<LearningItem>;
+  learningResource: Array<LearningItem>;
 }
+
 
 interface iSchemeData extends iScheme, Document {}
 
+
 const LearningItemSchema = new Schema<LearningItem>({
-  id: { type: String },
-  value: {
-    type: [String, Object] || String,  
-  },
+  id: { type: String, required: true },
+  value: { type: Schema.Types.Mixed, required: true }, 
 });
+
 
 const SchemeModel = new Schema<iSchemeData>(
   {
-    classType: {
-      type: String,
-      required: true,
-    },
-    weeks: {
-      type: String,
-      required: true,
-    },
-    topics: {
-      type: String,
-      required: true,
-    },
-    subject: {
-      type: String,
-      required: true,
-    },
-    term: {
-      type: String,
-      required: true,
-    },
-    learningObject: [LearningItemSchema],
+    classType: { type: String, required: true },
+    weeks: { type: String, required: true },
+    topics: [{ type: Schema.Types.Mixed, required: true }], 
+    subject: { type: String, required: true },
+    term: { type: String },
+    learningObject: [LearningItemSchema], 
     learningActivities: [LearningItemSchema],
     learningResource: [LearningItemSchema],
     embeddedCoreSkills: [LearningItemSchema],
   },
-  { timestamps: true }
+  { timestamps: true } 
 );
 
-export default model<iSchemeData>("schemes", SchemeModel);
+
+export default model<iSchemeData>("Scheme", SchemeModel);
